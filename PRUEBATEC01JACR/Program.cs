00fe21ca2cf1d41a0360ac421e82ffdc4cb5f3d3
Joseceron101 <ESFE4612024@outlook.com>
2024-02-21 11:40:04 -0600

    inyeccion de dependencias

--- PRUEBATEC01JACR/Program.cs
+++ PRUEBATEC01JACR/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<DbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+
+
 
 var app = builder.Build();
 
