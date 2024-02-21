4592618f2107615b69e7136607c173219694a08c
Joseceron101 <ESFE4612024@outlook.com>
2024-02-21 10:09:04 -0600

    Agregar archivos de proyecto.

---
+++ PRUEBATEC01JACR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRUEBATEC01JACR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
